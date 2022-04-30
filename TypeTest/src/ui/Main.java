d13855061339715b4dfd5589133d0c9a7a9dab5a
DNGBMBR <martintesar2002@gmail.com>
2022-04-30 02:05:06 -0700

    Added a state machine to the program.

--- TypeTest/src/ui/Main.java
+++ TypeTest/src/ui/Main.java
@@ -7,17 +7,5 @@ public class Main {
 
     public static void main(String[] args) {
         new TypeGUI();
-        String input = "";
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Typing Speed Test");
-        System.out.println("Type this: \n");
-        for(int i = 0; i < 3; i++) {
-            System.out.println("speed test");
-        }
-        System.out.println("\n");
-        System.out.println(scanner.nextLine());
-
-
-
     }
 }
--- TypeTest/src/ui/gui/TypeGUI.java
+++ TypeTest/src/ui/gui/TypeGUI.java
@@ -9,7 +9,9 @@ public class TypeGUI extends JFrame implements ActionListener {
     public static final int HEIGHT = 1000;
     public static final int WIDTH = 1000;
 
-    private JPanel typePanel;
+    private State currentState = State.PREPARING;
+    private JPanel topLayerPanel;
+    private JPanel[] panel = new JPanel[3];
 
     public TypeGUI() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -25,12 +27,48 @@ public class TypeGUI extends JFrame implements ActionListener {
 
     }
 
-    public void startPanel() {
-        typePanel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("Type Speed Test", SwingConstants.CENTER);
-        typePanel.add(label, BorderLayout.NORTH);
-        add(typePanel);
+    private void setState(State nextState) {
+        currentState = nextState;
+        CardLayout cl = (CardLayout) (topLayerPanel.getLayout());
+        cl.show(topLayerPanel, currentState.toString());
+    }
 
+    public void startPanel() {
+        topLayerPanel = new JPanel(new CardLayout());
+        add(topLayerPanel);
+
+        panel[0] = new JPanel(new BorderLayout());
+        JLabel labelType = new JLabel("Type Speed Test", SwingConstants.CENTER);
+        panel[0].add(labelType, BorderLayout.NORTH);
+        JButton buttonType = new JButton("Press me");
+        buttonType.addActionListener((e) -> {
+            setState(State.TYPING);
+        });
+        panel[0].add(buttonType, BorderLayout.CENTER);
+        topLayerPanel.add(panel[0], State.PREPARING.toString());
+
+        panel[1] = new JPanel(new BorderLayout());
+        JLabel labelOther = new JLabel("TYPE FAST", SwingConstants.CENTER);
+        panel[1].add(labelOther, BorderLayout.CENTER);
+        JButton buttonOther = new JButton("hahahaha I'm in pain");
+        buttonOther.addActionListener((e) -> {
+            setState(State.FINISHED);
+        });
+        panel[1].add(buttonOther, BorderLayout.CENTER);
+        topLayerPanel.add(panel[1], State.TYPING.toString());
+
+        panel[2] = new JPanel(new BorderLayout());
+        JLabel labelThird = new JLabel("Results", SwingConstants.CENTER);
+        panel[2].add(labelThird, BorderLayout.CENTER);
+        JButton buttonGOGOGO = new JButton("Your time is TIME");
+        buttonGOGOGO.addActionListener((e) -> {
+            setState(State.PREPARING);
+        });
+        panel[2].add(buttonGOGOGO, BorderLayout.CENTER);
+        topLayerPanel.add(panel[2], State.FINISHED.toString());
+
+        CardLayout cl = (CardLayout)(topLayerPanel.getLayout());
+        cl.show(topLayerPanel, currentState.toString());
     }
 
 
