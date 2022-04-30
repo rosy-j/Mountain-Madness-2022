6224a54d8042dda99be8ff7bc5213b5810109a8a
MatchaBubbleT <89042585+MatchaBubbleT@users.noreply.github.com>
2022-04-30 02:02:11 -0700

    add textarea

--- TypeTest/src/ui/gui/TypeGUI.java
+++ TypeTest/src/ui/gui/TypeGUI.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionListener;
 public class TypeGUI extends JFrame implements ActionListener {
     public static final int HEIGHT = 1000;
     public static final int WIDTH = 1000;
-
     private JPanel typePanel;
 
     public TypeGUI() {
@@ -27,8 +26,18 @@ public class TypeGUI extends JFrame implements ActionListener {
 
     public void startPanel() {
         typePanel = new JPanel(new BorderLayout());
-        JLabel label = new JLabel("Type Speed Test", SwingConstants.CENTER);
+        JLabel label = new JLabel("Type \"speed test\" as many times as you want,\n then hit ENTER", SwingConstants.CENTER);
+        label.setFont(new Font("Serif", Font.PLAIN, 35));
         typePanel.add(label, BorderLayout.NORTH);
+//        JLabel label2 = new JLabel("Type \"speed test\" as many times as you want,\n then hit ENTER"
+//                , SwingConstants.CENTER);
+//        label.setFont(new Font("Serif", Font.PLAIN, 20));
+//        label.setLocation(30, 30);
+//        typePanel.add(label2);
+        JTextArea textArea = new JTextArea();
+        textArea.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        textArea.setLineWrap(true);
+        typePanel.add(textArea, BorderLayout.CENTER);
         add(typePanel);
 
     }
