9ea01fa5932d85c6f7d7d5e9cf2a3fbfafee0fd3
DNGBMBR <martintesar2002@gmail.com>
2022-04-29 22:36:51 -0700

    Version is functional with half a bug. Added a form of error checking.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <time.h>
 
-#define BUFFER_SIZE 15
+#define BUFFER_SIZE 200
 #define NS_PER_S 1000000000L
 
 char str[12] = "speed test\n";
@@ -28,31 +28,36 @@ void sub_timespec(struct timespec *start, struct timespec *end, struct timespec 
 
 int main() {
 	char buffer[BUFFER_SIZE];
+	int c;
 	struct timespec start, end, delta;
+	char correct = 0;
 
-	printf("Starting speed test in:\n3\n");
-	sleep(1);
-	printf("2\n");
-	sleep(1);
-	printf("1\n");
+	printf("Starting speed test in 1 second\n");
 	sleep(1);
 	printf("GO! Type 'speed test' as fast as you can!\n");
 
 	clock_gettime(CLOCK_REALTIME, &start);
-	
-	fgets(buffer, BUFFER_SIZE, stdin);
 
-	clock_gettime(CLOCK_REALTIME, &end);
+	while (!correct) {
+		fgets(buffer, BUFFER_SIZE, stdin);
 
-	//makes everything lowercase for consistency
-	for (int i = 0; str[i]; i++) {
-		buffer[i] = tolower(buffer[i]);
+		clock_gettime(CLOCK_REALTIME, &end);
+
+		//makes everything lowercase for consistency
+		for (int i = 0; str[i]; i++) {
+			buffer[i] = tolower(buffer[i]);
+		}
+
+		if (strcmp(str, buffer) != 0) {
+			printf("You spelled 'speed test' wrong, try again!\n");
+		}
+		else {
+			correct = 1;
+		}
 	}
 
 	sub_timespec(&start, &end, &delta);
 
 	printf("Finished in %d.%d seconds.\n", (int) delta.tv_sec, (int) delta.tv_nsec / 1000000L);
-	if (strcmp(str, buffer) != 0) {
-		printf("bruh.");
-	}
+	
 }
